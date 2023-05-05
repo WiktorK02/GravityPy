5d0f79ca578d10f0bdf4dffe53ea465305985e62
wkidon <wiktorkidon@student.agh.edu.pl>
2023-05-05 13:48:23 +0200

    Added initial velocity, game pause, solved issue with moving particles, upgraded graphics

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ clock.tick(60)
 font = pygame.font.Font('fonts/minecraft_font.ttf', 16)
 
 # Backgorund image 
-background_image = pygame.image.load("images/space2.jpg").convert_alpha()
-background_image = pygame.transform.scale(background_image, (width, height))
+#background_image = pygame.image.load("images/space2.jpg").convert_alpha()
+#background_image = pygame.transform.scale(background_image, (width, height))
 
 opacity = 100
-background_image.set_alpha(opacity)
+#background_image.set_alpha(opacity)
 G = 100
 scale = 1.0
 
@@ -32,11 +32,9 @@ class Particle:
         self.color = color
         self.stats_text = None
         self.selected = None
-
     def apply_force(self, force):
         acceleration = force / self.mass
         self.velocity += acceleration
-
     def update(self):
         self.position += self.velocity
     
@@ -51,7 +49,6 @@ class Particle:
             stats_lines = [
                 f"Mass: {self.mass}",
                 f"Radius: {self.radius}",
-                f"Position: {round(self.position.x, 0), round(self.position.y, 0)}",
                 f"Velocity: {round(self.velocity.x, 2), round(self.velocity.y, 2)}"
             ]
             line_height = 20  
@@ -96,7 +93,7 @@ class Buttons:
         self.text = font.render(self.text_input, True, "white")
         self.text_rect = self.text.get_rect(center=(self.button_rect.center))
         self.surface = pygame.Surface(self.button_rect.size, pygame.SRCALPHA)  # Create a surface with transparency
-        color = (255, 60, 0, 128)  # Set the color with transparency
+        color = (100, 80, 90, 128)  # Set the color with transparency
         pygame.draw.rect(self.surface, color, (0, 0, *self.button_rect.size))  # Draw the rectangle on the surface
         screen.blit(self.surface, self.button_rect)  # Blit the surface onto the screen
         screen.blit(self.text, self.text_rect)
@@ -126,6 +123,12 @@ decrease_button_stateR = False
 increase_button_stateM = False
 decrease_button_stateM = False
 
+increase_button_stateVx = False
+decrease_button_stateVx = False
+
+increase_button_stateVy = False
+decrease_button_stateVy = False
+
 add_button_statement = False
 add_button_statement_put = False
 
@@ -133,27 +136,45 @@ reset_button_statement = False
 reset_scale_button_statement = False
 
 move_particles = False
+pause = False
 
 selected_particle = None
 
-# Radius, mass of added particle
+# Initial values 
 added_radius = 20
 added_mass = 20
+added_velocity = pygame.Vector2(0, 0)
 
-def decrese_radius():
+def decrease_radius():
     global added_radius
     if added_radius-1 != 0:
         added_radius -= 1
-def increse_radius():
+def increase_radius():
     global added_radius
     added_radius+= 1
-def decrese_mass():
+def decrease_mass():
     global added_mass
     if added_mass-1 != 0:
         added_mass -= 1
-def increse_mass():
+def increase_mass():
     global added_mass
     added_mass += 1
+def decrease_velocity_x():
+    global added_velocity
+    if not add_button_statement_put:
+        added_velocity.x -= 0.05
+def increase_velocity_x():
+    global added_velocity
+    if not add_button_statement_put:
+        added_velocity.x += 0.05
+def decrease_velocity_y():
+    global added_velocity
+    if not add_button_statement_put:
+        added_velocity.y -= 0.05
+def increase_velocity_y():
+    global added_velocity
+    if not add_button_statement_put:
+        added_velocity.y += 0.05
 def reset():
     global num_particles, particles, reset_button_statement 
     num_particles = 0
@@ -183,6 +204,14 @@ while running:
                     increase_button_stateM = True
                 elif button_decrese_m.button_rect.collidepoint(event.pos):
                     decrease_button_stateM = True
+                elif button_increse_vx.button_rect.collidepoint(event.pos):
+                    increase_button_stateVx = True
+                elif button_decrese_vx.button_rect.collidepoint(event.pos):
+                    decrease_button_stateVx = True
+                elif button_increse_vy.button_rect.collidepoint(event.pos):
+                    increase_button_stateVy = True
+                elif button_decrese_vy.button_rect.collidepoint(event.pos):
+                    decrease_button_stateVy = True
                 elif add_button.button_rect.collidepoint(event.pos):
                     add_button_statement = True
                 elif add_button_statement:
@@ -218,6 +247,10 @@ while running:
                 decrease_button_stateR = False
                 increase_button_stateM = False
                 decrease_button_stateM = False
+                increase_button_stateVx = False
+                decrease_button_stateVx = False
+                increase_button_stateVy = False
+                decrease_button_stateVy = False
                 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
@@ -232,23 +265,33 @@ while running:
             elif event.key == pygame.K_DOWN:
                 move_particles = True
                 move_direction = pygame.Vector2(0, -5)
+            elif event.key == pygame.K_SPACE:
+                pause = not pause
         elif event.type == pygame.KEYUP:
             if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]:
                 move_particles = False
 
     
     # Draw the background image on the screen
-    screen.fill((0,0,0))
-    screen.blit(background_image, (0, 0))
+    screen.fill((20,20,40))
+    #screen.blit(background_image, (0, 0))
 
     if increase_button_stateR:
-        increse_radius()
+        increase_radius()
     elif decrease_button_stateR:
-        decrese_radius()
+        decrease_radius()
     elif increase_button_stateM:
-        increse_mass()
+        increase_mass()
     elif decrease_button_stateM:
-        decrese_mass()
+        decrease_mass()
+    elif increase_button_stateVx:
+        increase_velocity_x()
+    elif decrease_button_stateVx:
+        decrease_velocity_x()
+    elif increase_button_stateVy:
+        increase_velocity_y()
+    elif decrease_button_stateVy:
+        decrease_velocity_y()
     elif reset_button_statement:
         reset()
     elif reset_scale_button_statement:
@@ -268,6 +311,7 @@ while running:
             )
 
     if add_button_statement_put:
+        particle_velocity = added_velocity.copy()
         particles.append(
             Particle(
                 mouse_xd,
@@ -275,44 +319,59 @@ while running:
                 added_mass,
                 added_color,
                 added_radius,
-                pygame.Vector2(0, 0)
+                particle_velocity
             )
         )
         add_button_statement_put = False
         added_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         num_particles += 1
-    
+
+    # Sort in case bigger particles do not cover smaller ones 
+    particles = sorted(particles, key=lambda x: x.radius, reverse=True)
     for particle in particles:
         if move_particles:
             particle.position += move_direction * scale
+        elif pause:
+            pass
         else:
             particle.update()
-        for other_particle in particles:
-            if particle != other_particle:
-                particle.apply_gravitational_force(other_particle, G)
+        
+            for other_particle in particles:
+                if particle != other_particle:
+                    particle.apply_gravitational_force(other_particle, G)
         particle.draw_scaled(mouse_x, mouse_y)
 
     # Buttons
-    button_increse_r = Buttons(50,50,"Increse r", pygame.Rect(50,50,120,25))
-    button_decrese_r = Buttons(150,50,"Decrese r", pygame.Rect(200,50,120,25))
-    button_increse_m = Buttons(250,50,"Increse m", pygame.Rect(400,50,120,25))
-    button_decrese_m = Buttons(350,50,"Decrese m", pygame.Rect(550,50,120,25))
-    add_button = Buttons(350,50,"Add", pygame.Rect(750,50,120,25))
-    reset_button = Buttons(350,50,"Reset", pygame.Rect(900,50,120,25))
+    button_increse_r = Buttons(50,50,"Increse R", pygame.Rect(50,50,120,25))
+    button_decrese_r = Buttons(150,50,"Decrese R", pygame.Rect(50,85,120,25))
+    button_increse_m = Buttons(250,50,"Increse M", pygame.Rect(200,50,120,25))
+    button_decrese_m = Buttons(350,50,"Decrese M", pygame.Rect(200,85,120,25))
+    button_increse_vx = Buttons(250,50,"+ Vx", pygame.Rect(350,50,50,25))
+    button_decrese_vx = Buttons(350,50,"- Vx", pygame.Rect(350,85,50,25))
+    button_increse_vy = Buttons(250,50,"+ Vy", pygame.Rect(410,50,50,25))
+    button_decrese_vy = Buttons(350,50,"- Vy", pygame.Rect(410,85,50,25))
+    add_button = Buttons(350,50,"Add", pygame.Rect(550,50,120,60))
+    reset_button = Buttons(350,50,"Reset Praticles", pygame.Rect(850,50,160,25))
     reset_scale_button = Buttons(350,50,"Reset scale", pygame.Rect(1050,50,120,25))
     
     # Text
-    text_mass = font.render(f'Mass: {added_mass}', True, (240,240,240))
-    screen.blit(text_mass, (400, 20))
-
     text_radius = font.render(f'Radius: {added_radius}', True, (240,240,240))
     screen.blit(text_radius, (50, 20))
+
+    text_mass = font.render(f'Mass: {added_mass}', True, (240,240,240))
+    screen.blit(text_mass, (200, 20))
+        
+    text_mass = font.render(f'Velocity: {added_velocity}', True, (240,240,240))
+    screen.blit(text_mass, (350, 20))
     
     text_num_partciles = font.render(f'Number of particles: {num_particles}', True, (240,240,240))
     screen.blit(text_num_partciles, (950, 0))
     
     text_num_partciles = font.render(f'Scale: {int(scale*100)}%', True, (240,240,240))
     screen.blit(text_num_partciles, (950, 20))
+    
+    text_num_partciles = font.render('Move camera with arrows keys, pause with a space', True, (240,240,240))
+    screen.blit(text_num_partciles, (width // 2 - 200, 0))
 
     pygame.display.flip()
 
